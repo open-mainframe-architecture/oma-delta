eec3e1e72bce5bb17cd0cf74f5097f381d39fa75
highdutch <dick.heijink@gmail.com>
2015-12-20 20:03:04 +0100

    Work in progress

---
+++ Pub.Oma.Delta.Core/cls/Pub/Oma/Delta/Session.js
@@ -0,0 +1,10 @@
+'Std.BaseObject+Std.Role'.subclass(function (I) {
+  "use strict";
+  I.peek({
+    rootValue: function () { },
+    sessionDirection: function () { }
+  });
+  I.play({
+    synchronizeModel: function () { }
+  });
+})
---
+++ Pub.Oma.Delta.Core/config.js
@@ -0,0 +1,4 @@
+function configure(module) {
+  "use strict";
+  module.description = 'This module implements core delta functionality.'
+}
