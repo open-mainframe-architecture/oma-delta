1a03167d59a3ec721b62edaba44fd65e1c3d1d96
highdutch <dick.heijink@gmail.com>
2016-07-30 00:51:29 +0200

    Work in progress

--- Std.Delta.API/cls/Std/Delta/RemoteSession.js
+++ Std.Delta.API/cls/Std/Delta/RemoteSession.js
@@ -1,4 +1,15 @@
+'RemoteRole'.subclass(I => {
   "use strict";
   I.play({
+    alloc: I.remotely('Delta.File => string', I.burdenSubclass),
+    do: I.remotely('Delta.Message => ', I.burdenSubclass),
+    dump: I.remotely('=> Delta.Dump', I.burdenSubclass),
+    greedy: I.remotely('=> number', I.burdenSubclass),
+    modeltype: I.remotely('=> string', I.burdenSubclass),
+    plumb: I.remotely('=> string', I.burdenSubclass),
+    poll: I.remotely('=> Delta.Status', I.burdenSubclass),
+    ready: I.remotely('=> number', I.burdenSubclass),
+    realloc: I.remotely('<Delta.File> =>', I.burdenSubclass),
+    typespace: I.remotely('=> <string>', I.burdenSubclass)
   });
 })
